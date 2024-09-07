40cf1fa26b094aa7fee2bcbc80e39337085b1840
znataniel <znataniel@protonmail.com>
2024-09-07 20:33:29 -0300

    qtile: emacs style kbinds to switch floatin windows

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -89,7 +89,11 @@ keys = [
     ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # For floating layouts: next and previous window
+    Key([mod, "control"], "p", lazy.group.prev_window(), desc="Previous window in group"),
+    Key([mod, "control"], "n", lazy.group.next_window(), desc="Next window in group"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
