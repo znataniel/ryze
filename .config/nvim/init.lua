e3d42cfc766ce8d095413a31ebedf051bdbe0f2d
znataniel <znataniel@protonmail.com>
2024-09-07 20:32:51 -0300

    nvim: add some language servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -508,6 +508,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        bashls = {},
+        ltex = {},
+
         clangd = {},
         gopls = {},
         pyright = {},
